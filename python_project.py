30b73c3ca09affc1cf7be5b1f8baade7c1d4842c
Akwana686 <akwanaoliver@gmail.com>
2026-04-15 22:01:20 +0300

    Done with two challenges

--- python_project.py
+++ python_project.py
@@ -1,6 +1,13 @@
 # Write a Python program that checks whether a number entered by the user is even or odd.
 number = int(input("Enter a number: "))
-if number % 2 == 0 and number % 2 != 0:
+if number % 2 == 0:
     print("The number is even.")
 else:
     print("The number is odd.")
+
+# Write a program that calculates the sum of numbers from 1 up to a given number.
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + i
+print(f"The sum from 1 to {n} is: {sum}")
